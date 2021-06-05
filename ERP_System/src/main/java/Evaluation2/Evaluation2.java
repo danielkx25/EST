3df8090df659e96d249603dc5145db194bc33d56
Daniel <36846400+danielkx25@users.noreply.github.com>
2021-06-04 23:45:24 -0300

    fix text

--- ERP_System/src/main/java/Evaluation2/Evaluation2.java
+++ ERP_System/src/main/java/Evaluation2/Evaluation2.java
@@ -27,7 +27,7 @@ public class Evaluation2 {
             System.out.println("1-Search Product by Hash Id");
             System.out.println("2-Search Product by Name");
             System.out.println("3-Remove Product by Hash Id");
-            System.out.println("4-Binary Search by Numeric Id");
+            System.out.println("4-Binary Tree Search by Numeric Id");
             System.out.println("5-End Evaluation");
             option = new Scanner(System.in).nextInt();
             switch(option){
@@ -50,7 +50,7 @@ public class Evaluation2 {
                     func.RemoveStockById(search,quantity);
                 break;
                 case 4:
-                    System.out.println("Type the product Numeric Id for binary search:");
+                    System.out.println("Type the product Numeric Id for binary tree search:");
                     searchInt = new Scanner(System.in).nextInt();
                     func.FindInBinaryTree(rootNode, searchInt);
                 break;
